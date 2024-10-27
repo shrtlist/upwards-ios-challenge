553ee3bcbd654925495f5cb79baafdd3c56db8c4
Marco Abundo <shrtlist@gmail.com>
2024-10-26 20:06:30 -0700

    Added album artwork

--- upwards-ios-challenge/Views/AlbumRowView.swift
+++ upwards-ios-challenge/Views/AlbumRowView.swift
@@ -9,15 +9,41 @@ import SwiftUI
 
 struct AlbumRowView: View {
     let album: Album
+    private let frameSize = 80.0
+    private let cornerRadius = 8.0
+    private let padding = 8.0
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text(album.name)
-                .font(.headline)
-            Text(album.artistName)
-                .font(.subheadline)
-                .foregroundColor(.secondary)
+        HStack {
+            if let artworkUrl = album.artworkUrl100,
+               let url = URL(string: artworkUrl) {
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: frameSize, height: frameSize)
+                        .cornerRadius(cornerRadius)
+                } placeholder: {
+                    ProgressView()
+                        .frame(width: frameSize, height: frameSize)
+                }
+            } else {
+                Image(systemName: "music.note")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: frameSize, height: frameSize)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(cornerRadius)
+            }
+
+            VStack(alignment: .leading, spacing: padding) {
+                Text(album.name)
+                    .font(.headline)
+                Text(album.artistName)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            .padding(.vertical, padding)
         }
-        .padding(.vertical, 8)
     }
 }
